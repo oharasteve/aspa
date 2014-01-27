3e0d8bf8347206c414730c2f3d3446c9d5b6ed57
srivatsb <bsrivats@gmail.com>
2014-01-27 01:10:27 -0800

    Use jinja2 for view
    Start using jinja2 for view.

--- main.py
+++ main.py
@@ -14,11 +14,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
 
+import os
+import urllib
+
+import jinja2
 import seasons
 import shared
 import view
+import webapp2
+
 
 class MainHandler(webapp2.RequestHandler):
   def get(self):
@@ -26,10 +31,19 @@ class MainHandler(webapp2.RequestHandler):
 
     # Show the webpage
     v = view.View()
-    v.header(self.response, season)
-    v.show(self.response, season)
-    shared.footer(self.response)
-      
+    v.showPage(JINJA_ENVIRONMENT, self.response, season)
+
+
+def datetimeformat(value, format='%b %d, %Y'):
+      return value.strftime(format)
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+JINJA_ENVIRONMENT.filters['datetimeformat'] = datetimeformat
+
 app = webapp2.WSGIApplication([
   (r'/', MainHandler)
 ], debug=True)
